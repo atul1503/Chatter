dbd89f2425a1cecc6da5dc5b342edaf2ad43a7b3
attripathi <attripathi@gainsight.com>
2024-08-24 19:36:20 +0530

    modified register for proper status code

--- src/main/java/com/chatter/Chatter/api/controller/UserController.java
+++ src/main/java/com/chatter/Chatter/api/controller/UserController.java
@@ -6,6 +6,10 @@ import org.apache.commons.logging.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,7 +44,7 @@ public class UserController {
 	
 	
 	@PostMapping("/register")
-	public DoneResponse register(@RequestBody Identify body) {
+	public ResponseEntity<DoneResponse> register(@RequestBody Identify body) {
 		
 		String username=body.getUsername();
 		String password=body.getPassword();
@@ -53,12 +57,12 @@ public class UserController {
 			ur.save(p);
 			response.setMessage("User added.");
 			response.setStatus(200);
-			return response;
+			return new ResponseEntity(response,HttpStatus.OK);
 			
 		}
 		response.setMessage("User already present.");
-		response.setStatus(305);
-		return response;
+		response.setStatus(304);
+		return new ResponseEntity<DoneResponse>(response,HttpStatus.NOT_MODIFIED);
 		
 	}
 
